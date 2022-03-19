c5d38cc1ebf8ee2e8666601ace95c6e485cd4c22
Nathan Reis <nathansilvareis@gmail.com>
2022-03-19 11:38:03 -0300

    Deixa listagem de LessonTypes 100% funcional

--- src/screens/lessonTypeList/index.tsx
+++ src/screens/lessonTypeList/index.tsx
@@ -1,11 +1,72 @@
-import { Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { Alert, Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { Button } from '../../components/button';
+import { Loading } from '../../components/loading';
 import { SafeZoneScreen } from '../../components/safeZoneScreen';
+import { Title } from '../../components/title';
+import { ILessonType } from '../../models/ILessonType';
+import { LessonTypeService } from '../../services/LessonTypeService';
 import styles from './styles';
 
 export function LessonTypeList() {
+  async function loadData() {
+    setIsLoading(true);
+
+    try {
+      let service = new LessonTypeService();
+
+      setLessonTypes(await service.getAll());
+    } catch {
+      Alert.alert('Ops', 'Erro inesperado!');
+    }
+
+    setIsLoading(false);
+  }
+
+  function handleEdit(id: number) {
+    navigation.navigate('Novo tipo de atividade' as never, { id } as never);
+  }
+
+  async function handleDelete(id: number) {
+    try {
+      let service = new LessonTypeService();
+
+      await service.delete(id);
+
+      await loadData();
+    } catch {
+      Alert.alert('Ops', 'Erro inesperado!');
+    }
+  }
+
+  let [lessonTypes, setLessonTypes] = useState<ILessonType[]>([]);
+  let [isLoading, setIsLoading] = useState<boolean>(true);
+  let navigation = useNavigation();
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <SafeZoneScreen>
-      <Text>Lista de tipos de atividades</Text>
+    <SafeZoneScreen isWithoutScroll={true}>
+      <Title content={'Tipos de\natividades'} />
+
+      <FlatList
+        data={lessonTypes}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.description}</Text>
+            <Button title='Editar' onPress={() => handleEdit(item.id!)} />
+            <Button title='Excluir' onPress={() => handleDelete(item.id!)} />
+          </View>
+        )} />
     </SafeZoneScreen>
   );
 }
