824abb5963e79fd27430ef7c8c835f81d474c178
Nathan Reis <nathansilvareis@gmail.com>
2022-03-19 16:31:43 -0300

    Cria componente Select para nputs de seleção

---
+++ src/components/select/index.tsx
@@ -0,0 +1,25 @@
+import { Picker } from '@react-native-picker/picker';
+import styles from './styles';
+
+interface ISelectProps {
+  data: { key: number | string, value: string }[];
+  selectedValue: number | string;
+  onValueChange: (value: any, index: number) => void;
+}
+
+export function Select(props: ISelectProps) {
+  let { data, selectedValue, onValueChange } = props;
+  let emptyValue = typeof data[0].key === 'number' ? 0 : '';
+
+  return (
+    <Picker
+      mode='dropdown'
+      selectedValue={selectedValue}
+      style={styles.picker}
+      onValueChange={onValueChange}
+    >
+      <Picker.Item key={emptyValue} label='' value={emptyValue} />
+      {data.map(item => <Picker.Item key={item.key} label={item.value} value={item.key} />)}
+    </Picker>
+  );
+}
